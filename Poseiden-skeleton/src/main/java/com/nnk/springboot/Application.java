12f98b85b794ea14d08337ae90c5bc277582bb2d
Ds_mohamed <smdioubate87@yahoo.com>
2022-05-07 12:50:49 +0000

    remove controller test

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/Application.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.Application"})
+//@ComponentScan(basePackages = {"com.example.Application"})
 public class Application {
 
 	public static void main(String[] args) {
